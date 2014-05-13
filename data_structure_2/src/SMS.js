0ebaeece0026c43fee194151c2d3c90c00120eba
zhaomingyuan7 <zhaomingyuan7@gmail.com>
2014-05-13 16:24:54 +0800

    save sign up name

--- data_structure_2/src/SMS.js
+++ data_structure_2/src/SMS.js
@@ -1,3 +1,15 @@
 function notify_sms_received(sms_json){
+    var bm_and_jj = sms_json.messages[0].message.substr(0, 2).replace(/\s/g, '')
+    var if_sign = {
 
+        BM: function(){
+            SignUp.save_sign_up_sms(sms_json)
+        },
+        JJ: function(){
+
+        }
+    }
+    if(if_sign[bm_and_jj]){
+        if_sign[bm_and_jj]();
+    }
 }
