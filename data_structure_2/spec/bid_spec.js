18fa443114a181159ff863c1f41b7a935c4a2a79
zhaomingyuan7 <zhaomingyuan7@gmail.com>
2014-05-15 17:49:09 +0800

    creat new bid

--- data_structure_2/spec/bid_spec.js
+++ data_structure_2/spec/bid_spec.js
@@ -24,11 +24,9 @@ describe("Bid", function () {
         Bidding.create_new_bid("0");
 
         var activities = JSON.parse(localStorage.activities);
-//        console.log(activities,'44444')
         expect(activities["0"].bids.length).toBe(1);
         expect(JSON.stringify(activities["0"].biddings["竞价1"])).toBe("[]");
-//        console.log(activities["0"].bids.length,'8888888')
-//        console.log(JSON.stringify(activities["0"].biddings["竞价1"]),'99999999')
+        console.log(JSON.stringify(activities["0"].biddings["竞价1"]),'99999999')
     });
 
 
--- data_structure_2/src/Bidding.js
+++ data_structure_2/src/Bidding.js
@@ -4,10 +4,7 @@ function Bidding(){
 Bidding.create_new_bid = function(name){
 
     var activities = JSON.parse(localStorage.activities);
-    var current_activities = _.filter(activities,function(activities){
-        return activities[0]
-    })
-    var bids = []
-    var biddings = {}
-    bids.push()
+    var bid_name = activities[name].bids.length+1
+    activities[name].bids.push('竞价'+bid_name)
+    localStorage.activities = JSON.stringify(activities);
 }
